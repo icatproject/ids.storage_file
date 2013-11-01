7b02fab790b27bd998791019f126936326d17a5e
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-11-01 09:35:58 +0000

    Inconsistent state with many errors

--- src/main/java/org/icatproject/ids/storage/MainFileStorage.java
+++ src/main/java/org/icatproject/ids/storage/MainFileStorage.java
@@ -3,14 +3,12 @@ package org.icatproject.ids.storage;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.zip.CRC32;
 
-import org.icatproject.Datafile;
 import org.icatproject.ids.plugin.DsInfo;
 import org.icatproject.ids.plugin.MainStorageInterface;
 import org.icatproject.utils.CheckedProperties;
@@ -77,145 +75,31 @@ public class MainFileStorage implements MainStorageInterface {
 	}
 
 	@Override
-	public InputStream get(DsInfo dsInfo) throws IOException {
-
-		return null;
+	public InputStream get(String location) throws IOException {
+		return Files.newInputStream(baseDir.resolve(location));
 	}
 
 	@Override
 	public boolean exists(DsInfo dsInfo) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
+		String location = dsInfo.getFacilityName() + "/" + dsInfo.getInvName() + "/"
+				+ dsInfo.getVisitId() + "/" + dsInfo.getDsName();
+		return Files.exists(baseDir.resolve(location));
 	}
 
-	// public InputStream getDataset(String location) throws IOException {
-	// File zippedDs = new File(new File(STORAGE_ZIP_DIR, location), "files.zip");
-	// if (!zippedDs.exists()) {
-	// throw new FileNotFoundException(zippedDs.getAbsolutePath());
-	// }
-	// return new BufferedInputStream(new FileInputStream(zippedDs));
-	// }
-
-	// public void putDataset(String location, InputStream is) throws IOException {
-	// File zippedDs = new File(new File(STORAGE_ZIP_DIR, location), "files.zip");
-	// File zippedDsDir = zippedDs.getParentFile();
-	// zippedDsDir.mkdirs();
-	// zippedDs.createNewFile();
-	// writeInputStreamToFile(zippedDs, is);
-	// }
-
-	// public void deleteDataset(String location) throws IOException {
-	// File zippedDs = new File(new File(STORAGE_ZIP_DIR, location), "files.zip");
-	// zippedDs.delete();
-	// }
-
-	// public boolean datasetExists(String location) throws IOException {
-	// File zippedDs = new File(new File(STORAGE_ZIP_DIR, location), "files.zip");
-	// return zippedDs.exists();
-	// }
-
-	// public InputStream getDatafile(String location) throws FileNotFoundException {
-	// if (STORAGE_DIR == null) {
-	// throw new UnsupportedOperationException(String.format(
-	// "Storage %s doesn't support single Datafiles", storageType));
-	// }
-	// File file = new File(STORAGE_DIR, location);
-	// if (!file.exists()) {
-	// throw new FileNotFoundException(file.getAbsolutePath());
-	// }
-	// return new BufferedInputStream(new FileInputStream(file));
-	// }
-
-	// public long putDatafile(String location, InputStream is) throws IOException {
-	// if (STORAGE_DIR == null) {
-	// throw new UnsupportedOperationException(String.format(
-	// "Storage %s doesn't support single Datafiles", storageType));
-	// }
-	// File file = new File(STORAGE_DIR, location);
-	// writeInputStreamToFile(file, is);
-	// return file.length();
-	// }
-
-	// public void deleteDatafile(String location) throws IOException {
-	// if (STORAGE_DIR == null) {
-	// throw new UnsupportedOperationException(String.format(
-	// "Storage %s doesn't support single Datafiles", storageType));
-	// }
-	// File file = new File(STORAGE_DIR, location);
-	// file.delete();
-	// }
-
-	// public boolean datafileExists(String location) throws IOException {
-	// if (STORAGE_DIR == null) {
-	// throw new UnsupportedOperationException(String.format(
-	// "Storage %s doesn't support single Datafiles", storageType));
-	// }
-	// File file = new File(STORAGE_DIR, location);
-	// return file.exists();
-	// }
-
-	// public InputStream getPreparedZip(String zipName) throws IOException {
-	// if (STORAGE_PREPARED_DIR == null) {
-	// throw new UnsupportedOperationException(String.format(
-	// "Storage %s doesn't support preparation of zip files for users", storageType));
-	// }
-	// File preparedZip = new File(STORAGE_PREPARED_DIR, zipName);
-	// if (!preparedZip.exists()) {
-	// throw new FileNotFoundException(preparedZip.getAbsolutePath());
-	// }
-	// return new BufferedInputStream(new FileInputStream(preparedZip));
-	// }
-
-	// public void putPreparedZip(String zipName, InputStream is) throws IOException {
-	// if (STORAGE_PREPARED_DIR == null) {
-	// throw new UnsupportedOperationException(String.format(
-	// "Storage %s doesn't support preparation of zip files for users", storageType));
-	// }
-	// File file = new File(STORAGE_PREPARED_DIR, zipName);
-	// writeInputStreamToFile(file, is);
-	// }
-
-	private void writeInputStreamToFile(File file, InputStream is) throws IOException {
-		File fileDir = file.getParentFile();
-		fileDir.mkdirs();
-
-		BufferedInputStream bis = null;
-		BufferedOutputStream bos = null;
-		try {
-			int bytesRead = 0;
-			byte[] buffer = new byte[BUFSIZ];
-			bis = new BufferedInputStream(is);
-			bos = new BufferedOutputStream(new FileOutputStream(file));
-
-			// write bytes to output stream
-			while ((bytesRead = bis.read(buffer)) > 0) {
-				bos.write(buffer, 0, bytesRead);
-			}
-		} finally {
-			if (bos != null) {
-				bos.close();
-			}
-		}
-	}
-
-	public boolean exists(Datafile df) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean exists(String location) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	@Override
 	public void delete(String location) throws IOException {
-		// TODO Auto-generated method stub
-
-	}
-
-	public InputStream get(String location) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		Path path = baseDir.resolve(location);
+		Files.delete(path);
+		/* Try deleting empty directories */
+		path = path.getParent();
+		try {
+			while (!path.equals(baseDir)) {
+				Files.delete(path);
+				path = path.getParent();
+			}
+		} catch (IOException e) {
+			// Directory probably not empty
+		}
 	}
 
 }
